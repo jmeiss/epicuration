c87a3909aa50a3ed5d376ed885261c23a8063303
Jonathan Meiss <jonathan@meiss.fr>
2013-11-01 23:36:26 +0100

    remove all presences of authentication_token and clean some code. Error for CSRF token still exists

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -3,10 +3,10 @@ class Api::V1::RegistrationsController < Api::V1::BaseController
   respond_to :json
   
   def create
-    user = User.new user_params
+    user = User.new safe_params
 
     if user.save
-      render json: user.as_json(auth_token: user.authentication_token, email: user.email), status: 201
+      render json: user.as_json(email: user.email, password: user.password), status: 201
       return
     else
       warden.custom_failure!
@@ -17,7 +17,7 @@ class Api::V1::RegistrationsController < Api::V1::BaseController
 
 private
 
-  def user_params
+  def safe_params
     params.require(:user).permit(:email, :password, :password_confirmation)
   end
 
--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::SessionsController < DeviseController
 
-  prepend_before_filter :require_no_authentication, :only => [:create]
+  prepend_before_filter :require_no_authentication, only: [:create]
   before_filter :ensure_params_exist
   respond_to :json
   
@@ -11,8 +11,7 @@ class Api::V1::SessionsController < DeviseController
  
     if resource.valid_password? params[:user][:password]
       sign_in 'user', resource
-      resource.ensure_authentication_token!
-      render json: { success: true, auth_token: resource.authentication_token, email: resource.email }
+      render json: { success: true, email: resource.email }, status: 201
       return
     end
 
@@ -23,17 +22,20 @@ class Api::V1::SessionsController < DeviseController
     resource = User.find_for_database_authentication(email: params[:user][:email])
     resource.authentication_token = nil
     resource.save
+
     render json: {success: true}
   end
  
 protected
   def ensure_params_exist
     return unless params[:user].blank?
+
     render json: {success: false, message: 'missing user parameter'}, status: 422
   end
  
   def invalid_login_attempt
     warden.custom_failure!
+
     render json: {success: false, message: 'Error with your email or password'}, status: 401
   end
 end
