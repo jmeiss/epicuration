8122d6c8dabbd688258173ce0b6e930e54a84a3b
Jonathan Meiss <jonathan@meiss.fr>
2013-11-01 15:23:58 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Epicuration::Application.config.secret_key_base = '747b8f044c58704a1606b8a3842f17c94b8954d2ae1d72ae976f6107edd2546993827df94b7537d4bd53e30af4515e710031862f911a213674b5d2614c70ad0c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Epicuration::Application.config.session_store :cookie_store, key: '_epicuration_session'
